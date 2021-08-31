e8edb2b4c5513d18d7ad861db10ccefa6973e929
Ricardo Asin <ricardoasinr@gmail.com>
2021-08-30 21:20:46 -0400

    First commit

---
+++ primero/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart'; //Primer paso
+
+void main() => runApp(MiApp()); //Segundo Paso
+
+class MiApp extends StatelessWidget {
+  const MiApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Mi app",
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatefulWidget {
+  Inicio({Key? key}) : super(key: key);
+
+  @override
+  _InicioState createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Ricardo App"),
+        ),
+        body: Center(
+          child: Text("Mi primera vez"),
+        ));
+  }
+}
