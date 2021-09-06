e35b63ed4b9a1debb6630caf63d00d214b751acf
Ricardo Asin <ricardoasinr@gmail.com>
2021-09-05 23:03:14 -0400

    Terminado

--- primer_json/lib/main.dart
+++ primer_json/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:primer_json/info.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'api_service.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -37,28 +39,128 @@ class _InicioState extends State<Inicio> {
   TextEditingController catController = TextEditingController();
 
   ApiService apiService = ApiService();
- SharedPreferences? sharedPreferences;
+  SharedPreferences? sharedPreferences;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text(
-        'Ricardo API',
-        style: TextStyle(color: Colors.white),
-      )),
+        title: Text(
+          'Ricardo Asin  - 2020113511',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.green,
+      ),
       body: Container(
-          color: Colors.white,
-          child: ListView(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            children: [
-              Column(
-                children: [
-                  SizedBox(height: 30.0),
-                ],
-              )
-            ],
-          )),
+        color: Colors.white,
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+          children: [
+            Column(
+              children: [
+                SizedBox(height: 30.0),
+                TextField(
+                  controller: apiNameController,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green,
+                      ),
+                    ),
+                    hintText: "Nombre API",
+                    fillColor: Colors.white,
+                    border: UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                TextField(
+                  controller: descController,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green,
+                      ),
+                    ),
+                    hintText: "Descripcion",
+                    border: UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                TextField(
+                  controller: linkController,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green,
+                      ),
+                    ),
+                    hintText: "Link",
+                    border: UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                TextField(
+                  controller: catController,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green,
+                      ),
+                    ),
+                    hintText: "Categoria",
+                    border: UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Container(
+                  width: 200.0,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      List<Info> data = await apiService.getInfo();
+                      getData(data, apiNameController, descController,
+                          linkController, catController);
+                    },
+                    child: Text('Obtener info'),
+                    style: ElevatedButton.styleFrom(primary: Colors.blue),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
+  }
+
+  getData(
+      List<Info> data,
+      TextEditingController name,
+      TextEditingController desc,
+      TextEditingController link,
+      TextEditingController cat) {
+    name.text = data.first.api.toString();
+    desc.text = data.first.description.toString();
+    link.text = data.first.link.toString();
+    cat.text = data.first.categoria.toString();
   }
 }
